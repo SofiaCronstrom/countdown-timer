d8826d97d86b43ff9cedaaca0c4bd12de24d0174
Sofia <sofia.cronstrom@gmail.com>
2024-08-29 16:52:08 +0200

    adding reset function and styling to timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Countdown from "./components/CountdownTimer";
-
+import "./App.css"
 
 export default function App(){
 
--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useRef, useState } from "react";
+import Play from "../assets/play.svg";
+import Pause from "../assets/pause.svg";
+import Reset from "../assets/reset.svg";
 
 export default function Countdown() {
-  const [timeLeft, setTimeLeft] = useState<number>(6);
+  const [timeLeft, setTimeLeft] = useState<number>(60);
   const [isActive, setIsActive] = useState<boolean>(false);
   //sätt ref för att har en referens till timern
   const timeRef = useRef<number | null>(null);
@@ -13,36 +16,55 @@ export default function Countdown() {
           if (prevTimeLeft === 0) {
             if (timeRef.current) {
               clearInterval(timeRef.current);
-              timeRef.current = null; 
+              timeRef.current = null;
             }
-            setIsActive(false);  // Stoppa timern
+            setIsActive(false); // Stoppa timern
             setTimeLeft(0);
           }
           return prevTimeLeft - 1;
         });
       }, 1000);
 
-      
       return () => {
         if (timeRef.current) {
           clearInterval(timeRef.current);
-          timeRef.current = null; // Reset the ref to null
+          timeRef.current = null;
         }
       };
     }
   }, [isActive, timeLeft]);
-   
+
+  const handleReset = () => {
+    if (timeRef.current) {
+      clearInterval(timeRef.current);
+      timeRef.current = null;
+    }
+    setTimeLeft(60);
+    setIsActive(false);
+  };
 
   return (
     <main>
-      <h1>Nedräkningstimer</h1>
-      <h2>{timeLeft} sekunder kvar</h2>
-      {/*knapp för att starta*/}
-      <button onClick={() => setIsActive(true)}>Starta</button>
-      {/*knapp för att pausa */}
-      <button onClick={() => setIsActive(false)}>Pausa</button>
-      {/*knapp för att återställa*/}
-      <button>Återställ</button>
+      
+      <section className="timer-container">
+        <article className="timeLeft-wrapper">
+          <h2>{timeLeft}</h2>
+        </article>
+        <article className="button-wrapper">
+          {/*knapp för att starta*/}
+          <button onClick={() => setIsActive(true)}>
+            <img src={Play} alt="Play button icon" />
+          </button>
+          {/*knapp för att pausa */}
+          <button onClick={() => setIsActive(false)}>
+            <img src={Pause} alt="Pause button icon" />
+          </button>
+          {/*knapp för att återställa*/}
+          <button onClick={handleReset}>
+            <img src={Reset} alt="Reset button icon" />
+          </button>
+        </article>
+      </section>
     </main>
   );
 }
